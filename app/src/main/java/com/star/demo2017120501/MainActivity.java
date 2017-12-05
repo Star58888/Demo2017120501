c29d83111701ec7d61557a85bebff147d81d2748
star <s0913940588@gmail.com>
2017-12-05 13:24:22 +0800

    改名，取得高度

--- app/src/main/java/com/star/demo2017120501/MainActivity.java
+++ app/src/main/java/com/star/demo2017120501/MainActivity.java
@@ -1,5 +1,6 @@
 package com.star.demo2017120501;
 
+import android.app.DownloadManager;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
@@ -9,21 +10,35 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener{
 
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
+    TextView tv2 ,tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv2 = findViewById(R.id.textView2);
+        tv = (TextView) findViewById(R.id.textView);
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
@@ -50,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         {
 // 取到手機地點後的程式碼
             Log.d("LOC", mLastLocation.getLongitude() + "," + mLastLocation.getLatitude());
+            tv2.setText("定位完成");
         }
     }
 
@@ -60,6 +76,38 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        tv2.setText("定位失敗");
 
+    }
+
+    public void click1(View v)
+    {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest("https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536,-104.9847034&key=AIzaSyBK2qv4njiVOpb8Sv6vtw6rLJCgemgM4N0",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            JSONArray array = obj.getJSONArray("results");
+                            JSONObject obj1 = array.getJSONObject(0);
+                            double result = obj1.getDouble("elevation");
+                            tv.setText(String.valueOf(result));
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
+        queue.start();
     }
 }
